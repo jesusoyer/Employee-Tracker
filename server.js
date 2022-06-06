7491951d18e888e3d6efa90997e1c64b04f2d26e
jesusoyer <oyervidesjesus017@gmail.com>
2022-06-06 16:54:04 -0500

    added to server.js

--- server.js
+++ server.js
@@ -21,22 +21,63 @@ const db = mysql.createConnection(
 
 
 
+// db.query('SELECT * FROM department', function(err, results){
+//     console.log(results)
+// })
 
-db.query('SELECT * FROM department', function(err, results){
-    console.log(results)
-})
-
-db.query('SELECT * FROM role', function(err, results){
-    console.log(results)
-})
-db.query('SELECT * FROM employee', function(err, results){
-    console.log(results)
-})
-db.query('SHOW TABLES', function(err, results){
-    console.log(results)
-})
+// db.query('SELECT * FROM role', function(err, results){
+//     console.log(results)
+// })
+// db.query('SELECT * FROM employee', function(err, results){
+//     console.log(results)
+// })
+// db.query('SHOW TABLES', function(err, results){
+//     console.log(results)
+// })
 
 
 app.listen(PORT, ()=> {
-    console.log(`server running on port ${PORT}`)
+     console.log(`server running on port ${PORT}`)
+    
 })
+
+
+function startApp(){inquirer.prompt([
+    
+  
+    {
+      type: 'list',
+      name: 'work_directory',
+      message: 'What would you like to do?',
+      default: 'none',
+      choices: [{key:"1" ,value:"View All Employees"},{key:2, value:"Add Employee"},{key:3,value:"Update Employee Role"},{key:4, value:"View All Roles"},{key:5, value:"Add Role"}, {key:6, value:"View All Departments"},{key:7, value:"Add Department"},{key:8, value:"Quit"}
+        
+      ],
+    },
+    // {
+    //   type: 'input',
+    //   name: 'features',
+    //   message: 'What features does your project have?',
+    // },
+   
+  ])
+  
+  .then((answers) => {
+    console.log(answers)
+    if(answers.work_directory==="View All Roles"){
+        db.query('SELECT * FROM role', function(err, results){
+     console.table(results)
+  })
+      } else if(answers.work_directory === "View All Departments"){
+        db.query('SELECT * FROM department', function(err, results){
+            console.table(results)
+         })
+      } else if(answers.work_directory === "View All Employees"){
+        db.query('SELECT * FROM employee', function(err, results){
+            console.table(results)
+         })
+
+      }
+  })
+}
+    startApp();

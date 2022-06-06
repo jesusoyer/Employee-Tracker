fa4952e2a22300cad8ae1e970316cba71768d354
jesusoyer <oyervidesjesus017@gmail.com>
2022-06-06 11:27:02 -0500

    fixed tables and installed inquirer

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2')
+const inquirer = require('inquirer')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -17,9 +18,24 @@ const db = mysql.createConnection(
     console.log('connected to work_db')
 );
 
+
+
+
+
 db.query('SELECT * FROM department', function(err, results){
     console.log(results)
 })
+
+db.query('SELECT * FROM role', function(err, results){
+    console.log(results)
+})
+db.query('SELECT * FROM employee', function(err, results){
+    console.log(results)
+})
+db.query('SHOW TABLES', function(err, results){
+    console.log(results)
+})
+
 
 app.listen(PORT, ()=> {
     console.log(`server running on port ${PORT}`)
